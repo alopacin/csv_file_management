de19f5e8cf2904126d06b338f58b9bf50687651a
alopacin <adlopacinski@gmail.com>
2023-06-20 22:43:05 +0200

    dodano obsluge trzech bledow tj. za malo podanych argumentow, bledna ilosc podanych argumentow dotyczaca zamiany wartosci i wpisane wspolrzedne ktore nie sa liczbami.usunieto warunek dotyczacy nazw plikow csv

--- reader.py
+++ reader.py
@@ -1,41 +1,43 @@
 import sys
 import csv
 
-# nadanie zmiennych, ktorych wartosci zostana wprowadzone z terminala
-file_in = sys.argv[1]
-file_out = sys.argv[2]
-change = sys.argv[3:]
-
-# jezeli uzytkownik poda za malo argumentow lub poda nieprawidlowe program zakonczy dzialanie
+# jezeli uzytkownik poda za malo argumentow program zakonczy dzialanie
 if len(sys.argv) < 2:
     print('Spróbuj jeszcze raz.Podałeś za mało danych')
     exit()
-elif file_in != 'in.csv':
-    print('Spróbuj jeszcze raz.Nieprawidłowa nazwa pliku')
-    exit()
-elif file_out != 'out.csv':
-    print('Spróbuj jeszcze raz.Nieprawidłowa nazwa pliku')
-    exit()
+
+# nadanie zmiennych, ktorych wartosci zostana wprowadzone z terminala
+file_in = sys.argv[1]
+file_out = sys.argv[2]
+changes = sys.argv[3:]
 
 data = []
 
-# odczyt danych z pliku in.csv
+# odczyt danych z pliku csv
 with open (file_in, 'r') as f:
     reader = csv.reader(f)
     for line in reader:
         data.append(line)
 
 # czesc odpowiadajaca za wyszukanie podanych wspolrzednych i zamianie wartosci
-for changes in change:
-    x, y, value = changes.split(',')
-    x = int(x)
-    y = int(y)
+for change in changes:
+    if len(change.split(',')) != 3:
+        print('Podałeś błędne dane.Spróbuj jeszcze raz')
+        exit()
+    x, y, value = change.split(',')
+    if not x.isdigit() or not y.isdigit():
+        print('Podałeś nieprawidłowe wartości współrzędnych.Spróbuj jeszcze raz')
+        exit()
+    else:
+        x = int(x)
+        y = int(y)
     if x >= len(data[0]) or y >= len(data):
         print('Podałeś za duże wartości współrzędnych')
         exit()
     data[x][y] = value
+print(f'Zmodyfikowany plik będzie miał następującą zawartość: \n{data}')
 
-# zapis do pliku out.csv
+# zapis do pliku csv
 with open (file_out, 'w', newline = '') as f:
     writer = csv.writer(f)
     for line in data:
